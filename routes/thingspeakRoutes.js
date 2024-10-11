d9b2a6850fd43a41d57fe7ba4606104499bdd295
markasilum <markasilum13@gmail.com>
2024-10-11 15:56:50 +0800

    get bus count api

--- routes/thingspeakRoutes.js
+++ routes/thingspeakRoutes.js
@@ -4,6 +4,8 @@ const thingSpeakController = require('../controllers/thingspeakControllers');
 
 router.get("/channel/:id", thingSpeakController.getRouteChannel);
 router.get("/passengers/route", thingSpeakController.getRoutePassengers);
+router.get("/passengers/route/bus/:id", thingSpeakController.getBusPassCountPerRoute);
+
 
 
 
