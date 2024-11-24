34916b25676f943b3bf7166f29aadf82c5462bce
markasilum <markasilum13@gmail.com>
2024-11-24 16:03:00 +0800

    null driver migration

--- prisma/migrations/20241014065616_null_driver/migration.sql
+++ prisma/migrations/20241014065616_null_driver/migration.sql
@@ -1,5 +1,2 @@
--- AlterTable
-ALTER TABLE `bus` MODIFY `driverId` VARCHAR(191) NULL;
-
 -- AddForeignKey
 ALTER TABLE `Bus` ADD CONSTRAINT `Bus_driverId_fkey` FOREIGN KEY (`driverId`) REFERENCES `Driver`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
