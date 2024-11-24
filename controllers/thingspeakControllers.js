ae1e5e1b8fa28759b5475a88e28d0d69eee5363b
markasilum <markasilum13@gmail.com>
2024-11-24 17:04:14 +0800

    response text

--- controllers/thingspeakControllers.js
+++ controllers/thingspeakControllers.js
@@ -122,7 +122,7 @@ const getRoutePassengers = async (req, res) => {
       try {
         const response = await fetch(thingspeakURL, { method: 'GET' });
         if (response.ok) {
-          console.log("route pass send response:", response);
+          console.log("route pass send response:", response.text());
         } else {
           console.log(`Failed to send total to ThingSpeak for route ${route.routeId}.`);
         }
